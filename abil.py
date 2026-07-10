ded667927f5cb4ce318776edb6baa551ba2d072e
Laura Gabriel <75498044+LauraGabrielSantes@users.noreply.github.com>
2026-07-10 03:17:52 -0600

    primera version

---
+++ abil.py
@@ -0,0 +1,88 @@
+#Implementa la simulacion del Algoritmo de inundación limitada usando como base el algoritmo de PI Segall
+import random
+from event import Event
+from model import Model
+
+
+class ABIL(Model):
+   contadorMensajes=0
+   
+   def init(self):
+     print ("\nInicio funciones", self.id)
+     print ("Mis vecinos son:", self.neighbors)
+     self.visitado=False
+     self.padre=self.id
+     self.TTL = 0
+     self.mis_recursos = self.llenaRecurso()
+     print ("[",self.id,"]: Mis recursos son: ", self.mis_recursos,"\n")
+
+   # Aqui se definen las acciones concretas que deben ejecutarse cuando se
+   # recibe un evento
+   def receive(self, event):
+    origen=event.getSource()
+    evento=event.getName()
+    if isinstance (evento,tuple):
+      nombre=evento[0]
+      listaMensaje = evento [1]
+      recursoBuscado = listaMensaje [0]
+      emisorBusqueda = listaMensaje [1]
+      TTL_recibido = listaMensaje [2]
+    else:
+      nombre = evento
+
+
+    if nombre == "INICIA":
+     self.visitado=True
+     emisorBusqueda = self.id
+     self.nodos_con_recurso = []
+     self.TTL = TTL_recibido
+     print ("\n[", self.id, "]: recibí INICIA y soy el nodo emisor de búsqueda con TTL ", self.TTL," en t=",self.clock," \n")
+     self.TTL -= 1
+     mensaje = ("M", [recursoBuscado,emisorBusqueda,self.TTL])
+     for vecino in self.neighbors:
+       newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
+       self.transmit(newevent)
+    elif nombre == "M":
+      ABIL.contadorMensajes+=1
+      print ("[", self.id, "]: recibí M de", origen," en t=",self.clock," \n")
+      if not self.visitado:
+        self.padre=origen
+        self.visitado=True
+        self.TTL = TTL_recibido
+        if self.localizaRecurso(self.mis_recursos, recursoBuscado):
+          newevent = Event("ENCONTRADO", self.clock + 1.0, emisorBusqueda, self.id)
+          self.transmit(newevent)
+        if self.TTL > 0:
+         self.TTL -= 1
+         mensaje = ("M",[recursoBuscado,emisorBusqueda,self.TTL])
+         for vecino in self.neighbors:
+          if vecino != self.padre:  
+            newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
+            self.transmit(newevent)
+      else:
+        if self.TTL < TTL_recibido:
+          self.TTL = TTL_recibido
+          mensaje = ("M",[recursoBuscado,emisorBusqueda,self.TTL])
+          for vecino in self.neighbors:
+            if vecino != origen: #evaluar si se descarta el envío al nodo padre, o se permite que se le envíe 
+              newevent = Event(mensaje, self.clock + 1.0, vecino, self.id)
+              self.transmit(newevent)
+    elif nombre == "ENCONTRADO":
+      self.nodos_con_recurso.append(origen)
+      print ("[", self.id,"]: El nodo ", origen," tiene el recurso  t = ",self.clock,"\nactualizo lista de nodos con recurso: ",self.nodos_con_recurso,"\n")
+
+   
+   def llenaRecurso(self):
+     listaRecursos =[]
+     tam = random.randint(2,6)
+     for i in range (0 , tam-1):
+       i = random.randint(1,10)
+       listaRecursos.append(i)
+     return listaRecursos
+   
+   def localizaRecurso(self, listaRecursos, recursoBuscado):
+     for i in listaRecursos:
+       if i == recursoBuscado:
+         return True
+     return False
+     
